e1358e0a8ae45f92bd92ea118f39fb433848d084
anton-sidelnikov <a.sidelnikov@t-systems.com>
2025-09-09 19:58:42 +0200

    pulumi example looping update

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -3,6 +3,9 @@
 import pulumi
 from pulumi_openstack import images, networking, compute
 
+cfg = pulumi.Config()
+instance_count = cfg.get_int("instance_count") or 2
+
 # External network lookup (router:external = true)
 ext_net = networking.get_network(external=True)
 
@@ -33,6 +36,13 @@ ri = networking.RouterInterface(
     subnet_id=subnet.id
 )
 
+# Find latest Debian public image
+image = images.get_image(
+    name_regex=r"(?i)debian*",
+    most_recent=True,
+    visibility="public",
+)
+
 # Security group allowing ICMP (ping)
 sg = networking.SecGroup(
     "workshop-sg",
@@ -47,44 +57,43 @@ icmp_ingress = networking.SecGroupRule(
     remote_ip_prefix="0.0.0.0/0",
     security_group_id=sg.id,
 )
+vms = []
+fips = []
+for i in range(instance_count):
 
-# Port on our subnet for the VM
-port = networking.Port(
-    "workshop-port",
-    network_id=net.id,
-    fixed_ips=[networking.PortFixedIpArgs(subnet_id=subnet.id)],
-    admin_state_up=True,
-    security_group_ids=[sg.id],
-)
+    # Port on our subnet for the VM
+    port = networking.Port(
+        f"workshop-port-{i}",
+        network_id=net.id,
+        fixed_ips=[networking.PortFixedIpArgs(subnet_id=subnet.id)],
+        admin_state_up=True,
+        security_group_ids=[sg.id],
+    )
 
-# Find latest Debian public image
-image = images.get_image(
-    name_regex=r"(?i)debian*",
-    most_recent=True,
-    visibility="public",
-)
+    # VM attached via the port
+    vm = compute.Instance(
+        f"workshop-vm-{i}",
+        flavor_name="s3.large.2",
+        image_id=image.id,
+        networks=[compute.InstanceNetworkArgs(port=port.id)],
+    )
+    vms.append(vm)
 
-# VM attached via the port
-vm = compute.Instance(
-    "workshop-vm",
-    flavor_name="s3.large.2",
-    image_id=image.id,
-    networks=[compute.InstanceNetworkArgs(port=port.id)],
-)
+    # Allocate and associate a Floating IP to the VM's port
+    fip = networking.FloatingIp(
+        f"workshop-fip-{i}",
+        port_id=port.id,
+        pool="admin_external_net"
+    )
+    fips.append(fip)
 
-# Allocate and associate a Floating IP to the VM's port
-fip = networking.FloatingIp(
-    "workshop-fip",
-    port_id=port.id,
-    pool="admin_external_net"
-)
-
-pulumi.export("vm_ip", vm.access_ip_v4)
+pulumi.export("vm_ip", [v.access_ip_v4 for v in vms])
+pulumi.export("floating_ips", [f.address for f in fips])
 pulumi.export("net_id", net.id)
 pulumi.export("subnet_id", subnet.id)
 pulumi.export("router_id", router.id)
 pulumi.export("external_net_id", ext_net.id)
-pulumi.export("floating_ip", fip.address)
 
 # Notes:
 # - ping floating_ip
+# - Configure count: pulumi config set example:instance_count 3
